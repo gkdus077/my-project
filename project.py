d8a6dc1467111e594c05a084e1411ab465602b39
gkdus077 <hayeon8520@gmail.com>
2024-10-26 09:44:57 +0900

    Add files via upload

--- project.py
+++ project.py
@@ -1,13 +1,13 @@
 import streamlit as st
 pages = {
     "회원" : [
-        st.Page("./page/a.py", title="공지사항📣"),
-        st.Page("./page/b.py", title="회원가입"),
-        st.Page("./page/c.py", title="로그인")
+        st.Page("a.py", title="공지사항📣"),
+        st.Page("b.py", title="회원가입"),
+        st.Page("c.py", title="로그인")
     ],
     "카테고리2" : [
-        st.Page("./page/d.py", title="신청방"),
-        st.Page("./page/e.py", title="playlist")
+        st.Page("d.py", title="신청방"),
+        st.Page("e.py", title="playlist")
     ]
 }
 pg = st.navigation(pages)
